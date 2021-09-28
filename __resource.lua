6d769e351698f6ba0d6b1c6516708def88b921ea
Grecha <41236228+Grecha99@users.noreply.github.com>
2021-09-28 14:02:24 +0200

    Base

---
+++ __resource.lua
@@ -0,0 +1,47 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+files {
+    'audio/sultanrsv8/sultanrsv8_game.dat151',
+    'audio/sultanrsv8/sultanrsv8_game.dat151.nametable',
+    'audio/sultanrsv8/sultanrsv8_game.dat151.rel',
+    'audio/sultanrsv8/sultanrsv8_sounds.dat54',
+    'audio/sultanrsv8/sultanrsv8_sounds.dat54.nametable',
+    'audio/sultanrsv8/sultanrsv8_sounds.dat54.rel',
+    'audio/sfx/dlc_v8sultanrs/v8sultanrs.awc',
+    'audio/sfx/dlc_v8sultanrs/v8sultanrs_npc.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/sultanrsv8/sultanrsv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/sultanrsv8/sultanrsv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_v8sultanrs'
+
+files {
+    'audio/turismoc/turismoc_game.dat151.rel',
+    'audio/turismoc/turismoc_sounds.dat54.rel',
+    'audio/sfx/dlc_turismoc/turismoc.awc',
+    'audio/sfx/dlc_turismoc/turismoc_npc.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/turismoc/turismoc_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/turismoc/turismoc_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_turismoc'
+
+
+files {
+    'audio/elegyx/elegyx_game.dat151',
+    'audio/elegyx/elegyx_game.dat151.nametable',
+    'audio/elegyx/elegyx_game.dat151.rel',
+    'audio/elegyx/elegyx_sounds.dat54',
+    'audio/elegyx/elegyx_sounds.dat54.nametable',
+    'audio/elegyx/elegyx_sounds.dat54.rel',
+    'audio/sfx/dlc_elegyx/elegyx.awc',
+    'audio/sfx/dlc_elegyx/elegyx_npc.awc',
+
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/elegyx/elegyx_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/elegyx/elegyx_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_elegyx'
+
+
+
