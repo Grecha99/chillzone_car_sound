e91f1bc8899a9e77a0e2a3bb7ca4dbc04cf0fd26
Grecha <krecha1999@gmail.com>
2021-09-28 15:42:50 +0200

    added new sounds

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,19 @@ games {'gta5'}
 
 author 'Grecha'
 description 'Car sounds for car pack'
-version '1.0.0'
+version '1.5.0'
 
+--[[
+Car sounds to use in meta audiohashname:
+
+sultanrsv8
+turismoc
+elegyx
+aventadorv12
+lfasound
+majimalm
+sentinelsg4
+--]]
 
 files {
     'audio/sultanrsv8/sultanrsv8_game.dat151',
@@ -62,10 +73,36 @@ files {
 	'audio/lfa/lfasound_game.dat151.rel',
 	'audio/lfa/lfasound_sounds.dat54.rel',
 	'audio/_sfx/dlc_lfasound/lfasound.awc',
-	'audio/_sfx/dlc_lfasound/lfasound_npc.awc'
+	'audio/_sfx/dlc_lfasound/lfasound_npc.awc',
 }
 data_file 'AUDIO_GAMEDATA' 'audio/lfa/lfasound_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audio/lfa/lfasound_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_lfasound'
 
 
+files {
+	'audio/majimagt/majimalm_game.dat151',
+	'audio/majimagt/majimalm_game.dat151.rel',
+    'audio/majimagt/majimalm_sounds.dat54',
+    'audio/majimagt/majimalm_sounds.dat54.rel',
+	'audio/_sfx/dlc_majimagt/majimagt.awc',
+	'audio/_sfx/dlc_majimagt/majimagt_npc.awc',
+}
+data_file 'AUDIO_GAMEDATA' 'audio/majimagt/majimalm_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/majimagt/majimalm_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_majimagt'
+
+
+files {
+    'audio/sentinelsg4/sentinelsg4_game.dat151',
+    'audio/sentinelsg4/sentinelsg4_game.dat151.nametable',
+    'audio/sentinelsg4/sentinelsg4_game.dat151.rel',
+    'audio/sentinelsg4/sentinelsg4_sounds.dat54',
+    'audio/sentinelsg4/sentinelsg4_sounds.dat54.nametable',
+    'audio/sentinelsg4/sentinelsg4_sounds.dat54.rel',
+    'audio/_sfx/dlc_sentinelsg4/sentinelsg4.awc',
+    'audio/_sfx/dlc_sentinelsg4/sentinelsg4_npc.awc',
+}
+data_file 'AUDIO_GAMEDATA' 'audio/sentinelsg4/sentinelsg4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/sentinelsg4/sentinelsg4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_sentinelsg4'
