e4e786b49aeecd559fb94f3fc226ed19bfb720cf
Grecha <krecha1999@gmail.com>
2021-09-28 14:54:54 +0200

    formating changes and added lfa

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ author 'Grecha'
 description 'Car sounds for car pack'
 version '1.0.0'
 
+
 files {
     'audio/sultanrsv8/sultanrsv8_game.dat151',
     'audio/sultanrsv8/sultanrsv8_game.dat151.nametable',
@@ -12,24 +13,24 @@ files {
     'audio/sultanrsv8/sultanrsv8_sounds.dat54',
     'audio/sultanrsv8/sultanrsv8_sounds.dat54.nametable',
     'audio/sultanrsv8/sultanrsv8_sounds.dat54.rel',
-    'audio/sfx/dlc_v8sultanrs/v8sultanrs.awc',
-    'audio/sfx/dlc_v8sultanrs/v8sultanrs_npc.awc',
+    'audio/_sfx/dlc_v8sultanrs/v8sultanrs.awc',
+    'audio/_sfx/dlc_v8sultanrs/v8sultanrs_npc.awc',
 }
-
 data_file 'AUDIO_GAMEDATA' 'audio/sultanrsv8/sultanrsv8_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audio/sultanrsv8/sultanrsv8_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_v8sultanrs'
+data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_v8sultanrs'
+
 
 files {
     'audio/turismoc/turismoc_game.dat151.rel',
     'audio/turismoc/turismoc_sounds.dat54.rel',
-    'audio/sfx/dlc_turismoc/turismoc.awc',
-    'audio/sfx/dlc_turismoc/turismoc_npc.awc',
+    'audio/_sfx/dlc_turismoc/turismoc.awc',
+    'audio/_sfx/dlc_turismoc/turismoc_npc.awc',
 }
-
 data_file 'AUDIO_GAMEDATA' 'audio/turismoc/turismoc_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audio/turismoc/turismoc_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_turismoc'
+data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_turismoc'
+
 
 files {
     'audio/elegyx/elegyx_game.dat151',
@@ -38,25 +39,33 @@ files {
     'audio/elegyx/elegyx_sounds.dat54',
     'audio/elegyx/elegyx_sounds.dat54.nametable',
     'audio/elegyx/elegyx_sounds.dat54.rel',
-    'audio/sfx/dlc_elegyx/elegyx.awc',
-    'audio/sfx/dlc_elegyx/elegyx_npc.awc',
+    'audio/_sfx/dlc_elegyx/elegyx.awc',
+    'audio/_sfx/dlc_elegyx/elegyx_npc.awc',
 }
-
 data_file 'AUDIO_GAMEDATA' 'audio/elegyx/elegyx_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audio/elegyx/elegyx_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_elegyx'
+data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_elegyx'
+
 
 files {
 	'audio/aventadorv12/aventadorv12_game.dat151.rel',
 	'audio/aventadorv12/aventadorv12_sounds.dat54.rel',
-	'audio/sfx/dlc_aventadorv12/aventadorv12.awc',
-	'audio/sfx/dlc_aventadorv12/aventadorv12_npc.awc',
+	'audio/_sfx/dlc_aventadorv12/aventadorv12.awc',
+	'audio/_sfx/dlc_aventadorv12/aventadorv12_npc.awc',
 }
-
 data_file 'AUDIO_GAMEDATA' 'audio/aventadorv12/aventadorv12_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audio/aventadorv12/aventadorv12_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_aventadorv12'
+data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_aventadorv12'
 
 
+files {
+	'audio/lfa/lfasound_game.dat151.rel',
+	'audio/lfa/lfasound_sounds.dat54.rel',
+	'audio/_sfx/dlc_lfasound/lfasound.awc',
+	'audio/_sfx/dlc_lfasound/lfasound_npc.awc'
+}
+data_file 'AUDIO_GAMEDATA' 'audio/lfa/lfasound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/lfa/lfasound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/_sfx/dlc_lfasound'
 
 
